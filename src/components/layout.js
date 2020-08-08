e243bf8a3f96e5d1b3d8dcf7da251643a2aefed0
Sirag Ahmed <ajj.siraj@gmail.com>
2020-08-08 15:32:11 +0200

    setup navbar and gsap animation

--- src/components/layout.js
+++ src/components/layout.js
@@ -5,43 +5,76 @@
  * See: https://www.gatsbyjs.org/docs/use-static-query/
  */
 
-import React from "react"
+import React, { useEffect } from "react"
 import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
 
 import Header from "./header"
 import "./layout.css"
+import { gsap } from "gsap"
+import ScrollTrigger from "gsap/ScrollTrigger"
 
-const Layout = ({ children }) => {
-  const data = useStaticQuery(graphql`
-    query SiteTitleQuery {
-      site {
-        siteMetadata {
-          title
-        }
-      }
+class Layout extends React.Component {
+  constructor({ children }) {
+    super({ children })
+  }
+
+  componentDidMount() {
+    console.log("IT iS RUNNING")
+    gsap.registerPlugin(ScrollTrigger)
+    gsap.from(".nav-link", {
+      alpha: 0,
+      scrollTrigger: { trigger: ".nav-link" },
+    })
+    if (document) {
+      gsap.from("#triggered", {
+        duration: 3,
+        alpha: 0,
+        scrollTrigger: { start: "50% 50%", markers: true },
+      })
     }
-  `)
+  }
+  // useEffect(() => {
 
-  return (
-    <>
-      <Header siteTitle={data.site.siteMetadata.title} />
-      <div
-        style={{
-          margin: `0 auto`,
-          maxWidth: 960,
-          padding: `0 1.0875rem 1.45rem`,
-        }}
-      >
-        <main>{children}</main>
-        <footer>
-          © {new Date().getFullYear()}, Built with
-          {` `}
-          <a href="https://www.gatsbyjs.org">Gatsby</a>
-        </footer>
-      </div>
-    </>
-  )
+  // })
+  render() {
+    return (
+      <>
+        
+        <nav className="navbar navbar-expand-lg navbar-light bg-light fixed-top">
+          <a className="navbar-brand" href="#triggered">
+            Navbar
+          </a>
+
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav mr-auto">
+              <li className="nav-item active">
+                <a className="nav-link" href="#triggered">
+                  Home <span className="sr-only">(current)</span>
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#triggered">
+                  Link
+                </a>
+              </li>
+              <li className="nav-item">
+                <a
+                  className="nav-link"
+                  href="#triggered"
+                  
+                >
+                  Link
+                </a>
+              </li>
+            </ul>
+          </div>
+        </nav>
+        <div style={{ minHeight: "100vh" }}>content</div>
+        <div style={{textAlign: "center", fontSize: "4rem", minHeight: "100vh"}} id="triggered">Scroll Triggered content</div>
+      </>
+    )
+  }
 }
 
 Layout.propTypes = {
